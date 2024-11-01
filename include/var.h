7be5503d6d5d18257f2dd9835095d74df9eabfe2
John Doe <johndoe@example.com>
2024-11-01 10:46:33 +0500

    aaa

---
+++ include/var.h
@@ -0,0 +1,38 @@
+#include "PCF8575.h"
+
+#define COLUMS           20   //LCD columns
+#define ROWS             4    //LCD rows
+#define LCD_SPACE_SYMBOL 0x20 //space symbol from LCD ROM, see p.9 of GDM2004D datasheet
+
+#define PCF8575_ADDRESS 0x26
+
+
+#define BUTTON1 P9
+#define BUTTON2 P10
+#define BUTTON3 P11
+#define BUTTON4 P12
+#define BUTTON5 P13
+#define BUTTON6 P14
+#define BUTTON7 P15
+
+#define START_BUTTON P8
+#define STOP_BUTTON  P7
+
+#define BUZZER_PIN 17
+
+#define LED1 26;
+#define LED2 27;
+#define LED3 14;
+#define LED4 13;
+#define LED5 18;
+#define LED6 23;
+#define LED7 19;
+
+#define POT1 36;
+#define POT2 39;
+#define POT3 34;
+#define POT4 35;
+#define POT5 32;
+#define POT6 33;
+#define POT7 25;
+
--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,9 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include "secrets.h"
-
-#define COLUMS           20   //LCD columns
-#define ROWS             4    //LCD rows
-#define LCD_SPACE_SYMBOL 0x20 //space symbol from LCD ROM, see p.9 of GDM2004D datasheet
-
-// #define WIFI_SSID "TP-Link_6A2A"
-// #define WIFI_PASSWORD "12624782"
-
-// #define API_KEY "AIzaSyCdZvy2cIOA-ULtQoiGMY25nBVod-CZ6rY"
-// #define USER_EMAIL "refill@mail.com"
-// #define USER_PASSWORD "refill"
-// #define DATABASE_URL "https://diastest-d6240-default-rtdb.europe-west1.firebasedatabase.app/"
-
-void asyncCB(AsyncResult &aResult);
-void printResult(AsyncResult &aResult);
+#include "var.h"
+#include "PCF8575.h"
+#include "FirebaseJson.h"
 
 DefaultNetwork network;
 UserAuth user_auth(API_KEY, USER_EMAIL, USER_PASSWORD);
@@ -36,6 +24,242 @@ unsigned long ms = 0;
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
 String deviceID;
+
+void asyncCB(AsyncResult &aResult);
+void printResult(AsyncResult &aResult);
+void beepBuzzer();
+void handleButtonClick(uint8_t pin);
+void displayScrollingText(const char* text, uint8_t row);
+object_t createInitVaraibles();
+void printAccount(String account);
+
+PCF8575 pcf8575(PCF8575_ADDRESS);
+
+const uint8_t buttonPins[] = {
+  BUTTON1, BUTTON2, BUTTON3, BUTTON4, BUTTON5, BUTTON6, BUTTON7, START_BUTTON, STOP_BUTTON
+};
+const uint8_t numButtons = sizeof(buttonPins) / sizeof(buttonPins[0]);
+
+bool buttonStates[numButtons];
+bool buttonLastStates[numButtons];
+
+unsigned long buzzerStartTime = 0;
+const unsigned long buzzerDuration = 100;
+bool buzzerOn = false;
+
+unsigned long lastButtonCheckTime = 0;
+const unsigned long buttonCheckInterval = 50;
+
+void setup()
+{
+
+    Serial.begin(115200);
+    lastButtonCheckTime = millis();
+    while (lcd.begin(COLUMS, ROWS, LCD_5x8DOTS) != 1) //colums, rows, characters size
+    {
+      Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal."));
+      delay(5000);   
+    }
+    lcd.print(F("PCF8574 is OK..."));
+    delay(2000);
+    lcd.clear();
+
+
+    lcd.setCursor(0, 0);
+    lcd.print("Connecting to WiFi");
+    displayScrollingText(WIFI_SSID, 1);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    int dotCount = 0;
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        lcd.setCursor(0, 2);
+        lcd.print("Status: ");
+        for (int i = 0; i <= dotCount; i++)
+        {
+            lcd.print(".");
+        }
+        dotCount = (dotCount + 1) % 10; // Обновление количества точек (0-9)
+        delay(500);
+    }
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("WiFi Connected!");
+    lcd.setCursor(0, 1);
+    lcd.print("IP:");
+    lcd.print(WiFi.localIP());
+    delay(2000);
+    lcd.clear();
+
+    deviceID = WiFi.macAddress();
+    deviceID.replace(":", "");
+    lcd.setCursor(0, 0);
+    lcd.print("Wifi MAC Address");
+    lcd.setCursor(0, 1);
+    lcd.print(deviceID);
+    delay(2000);
+    lcd.clear();
+    Serial.println(deviceID);
+
+    lcd.setCursor(0, 0);
+    lcd.print("Firebase Client");
+    lcd.setCursor(0, 1);
+    lcd.print("Version:");
+    lcd.print(FIREBASE_CLIENT_VERSION);
+    delay(2000);
+    lcd.clear();
+
+    lcd.setCursor(0, 0);
+    lcd.print("+7 702 313 20 61");
+    lcd.setCursor(0, 1);
+    lcd.print("Balance:");
+
+    for (uint8_t i = 0; i < numButtons; i++) {
+      pcf8575.pinMode(buttonPins[i], INPUT);
+      buttonStates[i] = false;
+      buttonLastStates[i] = false;
+    }
+    pinMode(BUZZER_PIN, OUTPUT);
+    digitalWrite(BUZZER_PIN, LOW);
+    
+
+    Firebase.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);
+    Serial.println("Initializing app...");
+    ssl_client1.setInsecure();
+    ssl_client2.setInsecure();
+    initializeApp(aClient1, app, getAuth(user_auth), asyncCB, "authTask");
+
+    // Binding the FirebaseApp for authentication handler.
+    // To unbind, use Database.resetApp();
+    app.getApp<RealtimeDatabase>(Database);
+    initializeApp(aClient2, app, getAuth(user_auth), asyncCB, "authTask");
+    Database.url(DATABASE_URL);
+    Database.setSSEFilters("get,put,patch,keep-alive,cancel,auth_revoked");
+    Database.get(aClient2, "/"+deviceID, asyncCB, true /* SSE mode (HTTP Streaming) */, "streamTask");
+
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  // The async task handler should run inside the main loop
+  // without blocking delay or bypassing with millis code blocks.
+  app.loop();
+  Database.loop();
+
+  if (app.ready() && !taskComplete){
+    Database.get(aClient2, "/"+deviceID + "/account", asyncCB, false, "getInitInfo");
+    taskComplete = true;
+  }
+
+  if(app.ready() && firstInit){
+    Database.set<object_t>(aClient2, "/", createInitVaraibles(), asyncCB, "setInitInfo");
+    lcd.setCursor(0, 3);
+    lcd.print("firstInit");
+    firstInit = false;
+  }
+
+
+  
+
+  // Проверяем кнопки каждые 50 мс
+  if (currentMillis - lastButtonCheckTime >= buttonCheckInterval) {
+    lastButtonCheckTime = currentMillis;
+
+    for (uint8_t i = 0; i < numButtons; i++) {
+      uint8_t pinValue = pcf8575.digitalRead(buttonPins[i]);
+      bool currentState = (pinValue == LOW);
+
+      if (currentState != buttonLastStates[i]) {
+        if (currentState == false) {
+          handleButtonClick(buttonPins[i]);
+        }
+        buttonLastStates[i] = currentState;  // Обновляем состояние кнопки
+      }
+    }
+  }
+  if (buzzerOn && (millis() - buzzerStartTime >= buzzerDuration)) {
+    digitalWrite(BUZZER_PIN, LOW);
+    buzzerOn = false;
+  }
+}
+
+void asyncCB(AsyncResult &aResult)
+{
+    // WARNING!
+    // Do not put your codes inside the callback and printResult.
+
+    printResult(aResult);
+}
+
+void printResult(AsyncResult &aResult)
+{
+    if (aResult.isEvent())
+    {
+        Firebase.printf("Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.appEvent().message().c_str(), aResult.appEvent().code());
+    }
+
+    if (aResult.isDebug())
+    {
+        Firebase.printf("Debug task: %s, msg: %s\n", aResult.uid().c_str(), aResult.debug().c_str());
+    }
+
+    if (aResult.isError())
+    {
+        Firebase.printf("Error task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.error().message().c_str(), aResult.error().code());
+    }
+
+    if (aResult.available())
+    {
+        RealtimeDatabaseResult &RTDB = aResult.to<RealtimeDatabaseResult>();
+        if (RTDB.isStream())
+        {
+            Serial.println("----------------------------");
+            Firebase.printf("task: %s\n", aResult.uid().c_str());
+            Firebase.printf("event: %s\n", RTDB.event().c_str());
+            Firebase.printf("path: %s\n", RTDB.dataPath().c_str());
+            Firebase.printf("data: %s\n", RTDB.to<const char *>());
+            Firebase.printf("type: %d\n", RTDB.type());
+
+            // The stream event from RealtimeDatabaseResult can be converted to the values as following.
+            bool v1 = RTDB.to<bool>();
+            int v2 = RTDB.to<int>();
+            float v3 = RTDB.to<float>();
+            double v4 = RTDB.to<double>();
+            String v5 = RTDB.to<String>();
+
+            String payload = RTDB.dataPath().c_str();
+            payload.trim();
+            if(payload == "/account"){
+              printAccount(RTDB.to<const char *>());
+            }
+            else if(payload == "/"){
+              Serial.println(RTDB.data());
+            }
+        }
+        else
+        {
+            Serial.println("----------------------------");
+            Firebase.printf("task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
+            String payload = aResult.c_str();
+            payload.trim();   
+            if(aResult.uid() == "getInitInfo" && payload == "null"){
+              firstInit = true;
+            }else if(aResult.uid() == "getInitInfo"){
+              lcd.setCursor(8, 1);
+              lcd.print(payload);
+            }
+        }
+
+#if defined(ESP32) || defined(ESP8266)
+        Firebase.printf("Free Heap: %d\n", ESP.getFreeHeap());
+#elif defined(ARDUINO_RASPBERRY_PI_PICO_W)
+        Firebase.printf("Free Heap: %d\n", rp2040.getFreeHeap());
+#endif
+    }
+}
+
 
 object_t createInitVaraibles(){
   JsonWriter writer;
@@ -99,180 +323,63 @@ void displayScrollingText(const char* text, uint8_t row)
     }
 }
 
-void setup()
+void handleButtonClick(uint8_t pin)
 {
+  beepBuzzer();
 
-    Serial.begin(115200);
-
-    while (lcd.begin(COLUMS, ROWS, LCD_5x8DOTS) != 1) //colums, rows, characters size
-    {
-      Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal."));
-      delay(5000);   
-    }
-    lcd.print(F("PCF8574 is OK..."));
-    delay(2000);
-    lcd.clear();
-
-
-    lcd.setCursor(0, 0);
-    lcd.print("Connecting to WiFi");
-    displayScrollingText(WIFI_SSID, 1);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-    int dotCount = 0;
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        lcd.setCursor(0, 2);
-        lcd.print("Status: ");
-        for (int i = 0; i <= dotCount; i++)
-        {
-            lcd.print(".");
-        }
-        dotCount = (dotCount + 1) % 10; // Обновление количества точек (0-9)
-        delay(500);
-    }
-
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("WiFi Connected!");
-    lcd.setCursor(0, 1);
-    lcd.print("IP:");
-    lcd.print(WiFi.localIP());
-    delay(2000);
-    lcd.clear();
-
-    deviceID = WiFi.macAddress();
-    deviceID.replace(":", "");
-    lcd.setCursor(0, 0);
-    lcd.print("Wifi MAC Address");
-    lcd.setCursor(0, 1);
-    lcd.print(deviceID);
-    delay(2000);
-    lcd.clear();
-    Serial.println(deviceID);
-
-    lcd.setCursor(0, 0);
-    lcd.print("Firebase Client");
-    lcd.setCursor(0, 1);
-    lcd.print("Version:");
-    lcd.print(FIREBASE_CLIENT_VERSION);
-    delay(2000);
-    lcd.clear();
-
-    lcd.setCursor(0, 0);
-    lcd.print("+7 702 313 20 61");
-    lcd.setCursor(0, 1);
-    lcd.print("Balance:");
-
-    Firebase.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);
-    Serial.println("Initializing app...");
-    ssl_client1.setInsecure();
-    ssl_client2.setInsecure();
-    initializeApp(aClient1, app, getAuth(user_auth), asyncCB, "authTask");
-
-    // Binding the FirebaseApp for authentication handler.
-    // To unbind, use Database.resetApp();
-    app.getApp<RealtimeDatabase>(Database);
-    initializeApp(aClient2, app, getAuth(user_auth), asyncCB, "authTask");
-    Database.url(DATABASE_URL);
-    Database.setSSEFilters("get,put,patch,keep-alive,cancel,auth_revoked");
-    Database.get(aClient2, "/"+deviceID, asyncCB, true /* SSE mode (HTTP Streaming) */, "streamTask");
-
-}
-
-void loop()
-{
-  // The async task handler should run inside the main loop
-  // without blocking delay or bypassing with millis code blocks.
-  app.loop();
-  Database.loop();
-
-  if (app.ready() && !taskComplete){
-    Database.get(aClient2, "/" + deviceID + "/account", asyncCB, false, "getInitInfo");
-    taskComplete = true;
+  if (pin == BUTTON1) {
+    Serial.println("Клик на BUTTON1");
+    // Ваш код здесь
   }
-
-  if(app.ready() && firstInit){
-    Database.set<object_t>(aClient2, "/", createInitVaraibles(), asyncCB, "setInitInfo");
-    lcd.setCursor(0, 3);
-    lcd.print("firstInit");
-    firstInit = false;
+  else if (pin == BUTTON2) {
+    Serial.println("Клик на BUTTON2");
+    // Ваш код здесь
   }
-
-  
-
+  else if (pin == BUTTON3) {
+    Serial.println("Клик на BUTTON3");
+    // Ваш код здесь
+  }
+  else if (pin == BUTTON4) {
+    Serial.println("Клик на BUTTON4");
+    // Ваш код здесь
+  }
+  else if (pin == BUTTON5) {
+    Serial.println("Клик на BUTTON5");
+    // Ваш код здесь
+  }
+  else if (pin == BUTTON6) {
+    Serial.println("Клик на BUTTON6");
+    // Ваш код здесь
+  }
+  else if (pin == BUTTON7) {
+    Serial.println("Клик на BUTTON7");
+    // Ваш код здесь
+  }
+  else if (pin == START_BUTTON) {
+    Serial.println("Клик на START_BUTTON");
+    // Ваш код здесь
+  }
+  else if (pin == STOP_BUTTON) {
+    Serial.println("Клик на STOP_BUTTON");
+    // Ваш код здесь
+  }
+  else {
+    Serial.print("Клик на неизвестной кнопке P");
+    Serial.println(pin);
+  }
 }
 
-void asyncCB(AsyncResult &aResult)
+void beepBuzzer()
 {
-    // WARNING!
-    // Do not put your codes inside the callback and printResult.
-
-    printResult(aResult);
+  digitalWrite(BUZZER_PIN, HIGH);
+  buzzerStartTime = millis();
+  buzzerOn = true;
 }
 
-void printResult(AsyncResult &aResult)
-{
-    if (aResult.isEvent())
-    {
-        Firebase.printf("Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.appEvent().message().c_str(), aResult.appEvent().code());
-    }
-
-    if (aResult.isDebug())
-    {
-        Firebase.printf("Debug task: %s, msg: %s\n", aResult.uid().c_str(), aResult.debug().c_str());
-    }
-
-    if (aResult.isError())
-    {
-        Firebase.printf("Error task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.error().message().c_str(), aResult.error().code());
-    }
-
-    if (aResult.available())
-    {
-        RealtimeDatabaseResult &RTDB = aResult.to<RealtimeDatabaseResult>();
-        if (RTDB.isStream())
-        {
-            Serial.println("----------------------------");
-            Firebase.printf("task: %s\n", aResult.uid().c_str());
-            Firebase.printf("event: %s\n", RTDB.event().c_str());
-            Firebase.printf("path: %s\n", RTDB.dataPath().c_str());
-            Firebase.printf("data: %s\n", RTDB.to<const char *>());
-            Firebase.printf("type: %d\n", RTDB.type());
-
-            // The stream event from RealtimeDatabaseResult can be converted to the values as following.
-            bool v1 = RTDB.to<bool>();
-            int v2 = RTDB.to<int>();
-            float v3 = RTDB.to<float>();
-            double v4 = RTDB.to<double>();
-            String v5 = RTDB.to<String>();
-            String payload = RTDB.dataPath().c_str();
-            payload.trim();
-            if(payload == "/account"){
-              lcd.setCursor(8, 1);
-              lcd.print(RTDB.to<const char *>());
-            }
-        }
-        else
-        {
-            Serial.println("----------------------------");
-            Firebase.printf("task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
-            String payload = aResult.c_str();
-            payload.trim();   
-            if(aResult.uid() == "getInitInfo" && payload == "null"){
-              firstInit = true;
-            }else if(aResult.uid() == "getInitInfo"){
-              lcd.setCursor(8, 1);
-              lcd.print(payload);
-            }
-        }
-
-#if defined(ESP32) || defined(ESP8266)
-        Firebase.printf("Free Heap: %d\n", ESP.getFreeHeap());
-#elif defined(ARDUINO_RASPBERRY_PI_PICO_W)
-        Firebase.printf("Free Heap: %d\n", rp2040.getFreeHeap());
-#endif
-    }
+void printAccount(String account){
+  beepBuzzer();
+  lcd.setCursor(8, 1);
+  lcd.print("          ");
+  lcd.setCursor(8, 1);
+  lcd.print(account);
 }
-
-
